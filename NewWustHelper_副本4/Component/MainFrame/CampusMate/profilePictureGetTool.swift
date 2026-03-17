aaf0a73a96052622deaa259cf02ecb7949f4ba26
JayCRL <2556693598@qq.com>
2026-03-17 18:51:03 +0800

    fix: 取消追踪WustChat等多余文件夹

--- NewWustHelper_副本4/Component/MainFrame/CampusMate/profilePictureGetTool.swift
+++
@@ -1,7 +0,0 @@
-//
-//  profilePictureGetTool.swift
-//  NewWustHelper
-//
-//  Created by wust_lh on 2025/9/18.
-//
-
--- WustChat/src/main/java/com/example/wustchat/WustchatFrame.java
+++
@@ -1,6 +0,0 @@
-package com.example.wustchat;
-
-final class WustchatFrame {
-    private WustchatFrame() {
-    }
-}
